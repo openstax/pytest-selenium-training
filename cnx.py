96db825e9755ccc5d56b84e9a18cedb95efa0e87
m1yag1 <mike.arbelaez@rice.edu>
2018-11-28 12:08:41 -0600

    added first test

--- cnx.py
+++ cnx.py
@@ -3,39 +3,38 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
+def test_search_query_in_url(driver, base_url):
+
+    # Bring up the page specified by the base_url
+    driver.get(base_url)
+
+    # Allow the page to load
+    sleep(3)
+
+    # Find the search input box and enter Biology and submit
+    search_box = driver.find_element_by_id('find-content-input')
+    search_box.send_keys('Biology')
+    search_box.send_keys(Keys.ENTER)
+
+    # Make an assertion that Biology is in the current url
+    try:
+        assert 'q=Biology' in driver.current_url
+    except AssertionError:
+        return 'Failed'
+
+    # Close down the browser
+    driver.quit()
+
+    return 'Passed'
+
+
 if __name__ == '__main__':
 
     # Create an instance of selenium web driver
     driver = webdriver.Chrome()
 
-    # Bring up the main cnx.org website
-    driver.get('https://cnx.org')
+    # Run our first test!
+    result = test_search_query_in_url(driver, 'https://cnx.org/browse')
 
-    sleep(3)
-
-    # Find the link to visit the search page
-    search_button = driver.find_element_by_css_selector('#page-nav #nav-browse a')
-
-    # Slow things down a bit
-    sleep(2)
-
-    # Click the link
-    search_button.click()
-
-    # Let the user see what is happening
-    sleep(5)
-
-    search_box = driver.find_element_by_id('find-content-input')
-    search_box.send_keys('Biology')
-
-    # Let the user see something
-    sleep(2)
-
-    # Submit the query
-    search_box.send_keys(Keys.ENTER)
-
-    # zzzzzz
-    sleep(5)
-
-    # Close down the browser
-    driver.quit()
+    print(f'The test {result}!')
