414431c670dca171dc8a15232b471a4b758e9d55
m1yag1 <mike.arbelaez@rice.edu>
2019-02-12 16:21:51 -0600

    added second test for no results and no result message

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,3 +1,5 @@
+import random
+from string import digits, ascii_letters
 from time import sleep
 
 import pytest
@@ -20,3 +22,35 @@ def test_search_query_in_url(selenium, base_url):
 
     # Make an assertion that Biology is in the current url
     assert 'q=Biology' in selenium.current_url
+
+
+@pytest.mark.nondestructive
+def test_search_returns_no_results_and_message_displayed(selenium, base_url):
+
+    # Bring up the page specified by the base_url
+    selenium.get(f'{base_url}/browse')
+
+    # Allow page to load
+    sleep(3)
+
+    # Create a string of random letters and numbers
+    query = ''.join(random.choice(digits + ascii_letters) for i in range(32))
+
+    # Find the search input box and enter the query and submit
+    search_box = selenium.find_element_by_id('find-content-input')
+    search_box.send_keys(query)
+    search_box.send_keys(Keys.ENTER)
+
+    # Allow enough time for results to display
+    sleep(3)
+
+    # Results are shown in a table element. Find all table rows.
+    result_items = selenium.find_elements_by_css_selector(
+        '#results table.table tbody tr')
+
+    # When no results are found a message is displayed. Find the message.
+    result_text = selenium.find_element_by_css_selector(
+        '#results p[data-l10n-id="search-results-list-no-results"]').text
+
+    assert len(result_items) == 0
+    assert result_text == "No results found. Please try expanding your search."
